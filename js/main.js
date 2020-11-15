a4c582dc676d2bdfedcb36a2dfbd109bc1173fe3
Roman <dadfrontend@gmail.com>
2020-11-15 21:05:49 +0300

    header line

---
+++ js/main.js
@@ -0,0 +1,11 @@
+document.querySelector(".hamburger-wraper").onclick = function(){
+  document.querySelector(".hamburger__menu").classList.toggle("hamburger__menu--active");
+  document.querySelector(".hamburger__container").classList.toggle("hamburger__container--active");
+}
+
+
+
+
+
+
+
