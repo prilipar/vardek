d646b9cdf4890cf86f1f8f69319db55956717a15
Roman <dadfrontend@gmail.com>
2020-11-15 23:44:49 +0300

    hero content

--- js/main.js
+++ js/main.js
@@ -2,8 +2,7 @@ document.querySelector(".hamburger-wraper").onclick = function(){
   document.querySelector(".hamburger__menu").classList.toggle("hamburger__menu--active");
   document.querySelector(".hamburger__container").classList.toggle("hamburger__container--active");
   
-  document.querySelector(".hamburger__container").classList.add('animate__animated', 'animate__bounceInDown');
-}
+ }
 
 
 
