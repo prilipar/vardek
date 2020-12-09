95a96e8ed88b3b5aa05931ede81c2baeae86cd96
Roman <dadfrontend@gmail.com>
2020-12-10 00:39:13 +0300

    slider

--- js/main.js
+++ js/main.js
@@ -2,6 +2,6 @@ document.querySelector(".hamburger-wraper").onclick = function(){
   document.querySelector(".hamburger__menu").classList.toggle("hamburger__menu--active");
   document.querySelector(".hamburger__container").classList.toggle("hamburger__container--active");
   
- }
+ };
 
  
